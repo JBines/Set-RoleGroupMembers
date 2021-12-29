89ccfa8df23faf48778caf9087577e931905cdfe
Joshua Bines <40031426+JBines@users.noreply.github.com>
2021-12-29 14:29:21 +0100

    Update Set-RoleGroupMembers.ps1

--- Set-RoleGroupMembers.ps1
+++ Set-RoleGroupMembers.ps1
@@ -5,38 +5,25 @@ This script automates the population of members from one group as members on CUS
 .DESCRIPTION
 This has been created with the primary goal of allowing scoped administrators to manage Exchange permissions on a subset of users.  
 
-## Set-RoleGroupMembers [-OwnerSourceGroup <string[ObjectID]>] [-RoleGroupsIdentity <Array[GUID]>] 
+## Set-RoleGroupMembers [-OwnerSourceGroup <string[ObjectID]>] [-RoleGroupsIdentity <Array[GUID]>] [-EXOOrganization <string[*.onmicrosoft.com]>] [-EXOAutomationPSConnection <string[Name]>] [-AADAutomationPSConnection <string[Name]>] 
 
 .PARAMETER OwnerSourceGroup
 The OwnerSourceGroup parameter details the ObjectId of the Azure Group which contains all the desired owners as members of one group.
 
 .PARAMETER RoleGroupsIdentity
-The RoleGroupsIdentity parameter specifies the role groups whose membership you want to modify. 
+The RoleGroupsIdentity parameter specifies the role groups whose membership you want to modify. This can be found by running Get-RoleGroup <Name> | FL Name,*guid*
 
 .PARAMETER DifferentialScope
 The DifferentialScope parameter defines how many objects can be added or removed from the UserGroups in a single operation of the script. The goal of this setting is throttle bulk changes to limit the impact of misconfiguration by an administrator. What value you choose here will be dictated by your userbase and your script schedule. The default value is set to 10 Objects. 
 
-.PARAMETER AutomationPSCredential
- The AutomationPSCredential parameter defines which Azure Automation Cred you would like to use. This is for Basic Auth Requirements consider updating this to use Service Principles instead. EXO expires this in Mid 2021. 
-
-.PARAMETER EXOAutomationCertificate
- The EXOAutomationCertificate parameter defines which Azure Automation certificate you would like to use which grants access to the Exchange Online App. This certificate must be already installed on the automation account in the certificate store.  
-
-.PARAMETER EXOAppId
-The EXOAppId parameter identifies the Azure Application ID GUID of the Service Principle Name. Parameter must be used with -AutomationCertificate. 
-
 .PARAMETER EXOOrganization
-The CertificateOrganization parameter identifies the tenant Microsoft address. For example 'consto.onmicrosoft.com' Parameter must be used with -EXOAutomationCertificate. 
+The CertificateOrganization parameter identifies the tenant Microsoft address. For example 'consto.onmicrosoft.com' Parameter must be used with -EXOAutomationPSCertificate & -EXOAutomationPSConnection. 
 
-.PARAMETER AzureADAutomationCertificate
- The EXOAutomationCertificate parameter defines which Azure Automation Certificate you would like to use which grants access to Exchange Online. 
+.PARAMETER EXOAutomationPSConnection
+ The AutomationPSConnection parameter defines the connection details such as AppID, Tenant ID. Parameter must be used with -EXOAutomationPSCertificate & -EXOOrganization.
 
-.PARAMETER AzureADAppId
-The EXOAppId parameter specifies the application ID of the service principal. Parameter must be used with -AzureADAutomationCertificate. 
-
-.PARAMETER AzureADTenantId
-The AzureADTenantId parameter You must specify the TenantId parameter to authenticate as a service principal or when using Microsoft account. Populate by using the Tenant GUID. 
-
+.PARAMETER AADAutomationPSConnection
+ The AutomationPSConnection parameter defines the connection details such as AppID, Tenant ID. Parameter must be used with -AADAutomationPSCertificate.
 
 .EXAMPLE
 Set-RoleGroupMembers -OwnerSourceGroup '7b7c4926-c6d7-4ca8-9bbf-5965751022c2' -RoleGroupsIdentity '0e55190c-73ee-e811-80e9-005056a31be6'
@@ -86,7 +73,8 @@ App Only Auth requires registering the Azure Application along with installing t
 
 Please note, when using Azure Automation with more than one user group the array should be set to JSON for example ['ObjectID','ObjectID']
 
-We used AzureADPreview 2.0.2.89 when testing this script. 
+We used AzureAD GA Version: 2.0.2.140 when testing this script.
+
 
 [AUTHOR]
 Joshua Bines, Consultant
@@ -101,11 +89,14 @@ Find me on:
 0.0.2 20190314 - JBines - [BUGFIX] Removed Secure String and Begin Process End as it is not supported in azure automation. 
                         - [Feature] Added a write-output when AutomationPSCredential is using in the write-log function
 1.0.0 20190314 - JBines - [MAJOR RELEASE] Other than that it works like a dream... 
-1.0.1 20191001 - CGarvin - Changed variable $OwnerSourceGroup from String type to $OwnerSourceGroups of Array type for maximum flexibility.
+1.0.1 20191001 - CG     - Changed variable $OwnerSourceGroup from String type to $OwnerSourceGroups of Array type for maximum flexibility.
 1.0.2 20191021 - JBines - [BUGFIX] Added Select-Object -Unique on the $RoleGroupsIdentity Array.
 1.0.3 20210106 - JBINES - [Feature] Added support for the use of Service Principles using Certificates based authenication for Exchange and Azure AD. Also updated AzureADPreview to 2.0.2.89
+1.0.4 20211227 - JBines - [Feature] Added switches for the Get-AutomationConnection and removed extra variables which were needed. Upgraded to AzureAD GA
+
 
 [TO DO LIST / PRIORITY]
+    Migrate to Graph API / MED
 
 #>
 
@@ -126,24 +117,14 @@ Param
     #[Parameter(ParameterSetName='EXO',Mandatory = $False)]
     [Parameter(Mandatory = $False)]
     [ValidateNotNullOrEmpty()]
-    [String]$EXOAutomationCertificate,
-    #[Parameter(ParameterSetName='EXO',Mandatory=$True)]
-    [ValidateNotNullOrEmpty()]
-    [String]$EXOAppId,
+    [String]$EXOAutomationPSConnection,
     [Parameter(Mandatory = $False)]
     [ValidatePattern('(?i)\S\.onmicrosoft.com$')]
     [ValidateNotNullOrEmpty()]
-    [String]$EXOCertificateOrganization,
-    #[Parameter(ParameterSetName='AzureAD',Mandatory = $False)]
+    [String]$EXOOrganization,
     [Parameter(Mandatory = $False)]
     [ValidateNotNullOrEmpty()]
-    [String]$AzureADAutomationCertificate,
-    #[Parameter(ParameterSetName='AzureAD',Mandatory=$True)]
-    [ValidateNotNullOrEmpty()]
-    [String]$AzureADAppId,
-    #[Parameter(ParameterSetName='AzureAD',Mandatory=$True)]
-    [ValidateNotNullOrEmpty()]
-    [String]$AzureADTenantId
+    [String]$AADAutomationPSConnection
 )
 
     #Set VAR
@@ -194,11 +175,15 @@ Param
                   {
                          Write-Host "[$TimeStamp] [$LogLevel] :: $Message" -ForegroundColor $Color
 
-                         if($AutomationPSCredential -or $EXOAutomationCertificate -or $AzureADAutomationCertificate)
+                         if($AutomationPSCredential -or $EXOAutomationPSConnection -or $AADAutomationPSConnection)
                          {
                          Write-Output "[$TimeStamp] [$LogLevel] :: $Message"
                     } 
                   }
+                  if($LogLevel -eq "ERROR")
+                    {
+                        Write-Error "[$TimeStamp] [$LogLevel] :: $Message"
+                    }
            }
     }
 
@@ -206,34 +191,20 @@ Param
 
     Try{
 
-        If($EXOAutomationCertificate){
-                
-            $EXOCert = Get-AutomationCertificate -Name $EXOAutomationCertificate
-
-            #Connect-ExchangeOnline -CertificateThumbprint $EXOCert.Thumbprint -AppId $EXOAppId -ShowBanner:$false -Organization $EXOCertificateOrganization
-            Connect-ExchangeOnline -CertificateThumbprint $EXOCert.Thumbprint -AppId $EXOAppId -Organization $EXOCertificateOrganization
-
-        }
-        else {
-            Remove-Variable EXOAutomationCertificate
-            Remove-Variable EXOAppId
-            Remove-Variable EXOCertificateOrganization
-        }
-
-        If($AzureADAutomationCertificate){
+        If($EXOAutomationPSConnection -and $AADAutomationPSConnection){
             
-            $AzureADCert = Get-AutomationCertificate -Name $AzureADAutomationCertificate
-
-            Connect-AzureAD -TenantId $AzureADTenantId -ApplicationId  $AzureADAppId -CertificateThumbprint $AzureADCert.Thumbprint  
+            #Connect Exchange
+            $EXOConnection = Get-AutomationConnection -Name $EXOAutomationPSConnection
+            #Connect-ExchangeOnline -CertificateThumbprint $EXOCert.Thumbprint -AppId $EXOAppId -ShowBanner:$false -Organization $EXOCertificateOrganization
+            Connect-ExchangeOnline -CertificateThumbprint $EXOConnection.CertificateThumbprint -AppId $EXOConnection.ApplicationId -Organization $EXOOrganization -ShowBanner:$false
+            
+            #Connect Azure AD
+            $AzureADConnection = Get-AutomationConnection -Name $AADAutomationPSConnection
+            Connect-AzureAD -TenantId $AzureADConnection.TenantId -ApplicationId $AzureADConnection.ApplicationId -CertificateThumbprint $AzureADConnection.CertificateThumbprint 
 
         }
-        else {
-            Remove-Variable AzureADAutomationCertificate
-            Remove-Variable AzureADAppId
-            Remove-Variable AzureADTenantId
-        }
 
-        if ($AutomationPSCredential) {
+        if ($AutomationPSCredential -and (-not($EXOAutomationPSConnection -and $AADAutomationPSConnection))) {
             
             $Credential = Get-AutomationPSCredential -Name $AutomationPSCredential
 
